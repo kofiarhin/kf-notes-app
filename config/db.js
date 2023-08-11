5cc423680c8dc49b311ad9a62e277ea44f712d12
kofi <kofiarhin69@gmail.com>
2023-08-11 13:39:36 +0100

    update: index view

--- config/db.js
+++ config/db.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const connect = async () => {
+  const dbURl =
+    process.env.NODE_ENV === "development"
+      ? process.env.MONGO_URI_DEVELOPMENT
+      : process.env.MONGO_URI_PRODUCTION;
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
+    const conn = await mongoose.connect(dbURl);
     console.log(`connected to databe: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
